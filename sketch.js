b9499fced63b784c8b4b7b5e0b25379b5948f360
Priyanshu Dasgupta <62264051+priyanshudasgupta8@users.noreply.github.com>
2020-05-17 22:06:12 -0700

    Removal of ONLY bug

--- sketch.js
+++ sketch.js
@@ -29,14 +29,17 @@ function draw() {
     var deformation = 0.5 * weight * speed * speed/25509;
     if (deformation > 180) {
       car.shapeColor = color(255, 0, 0);
+      wall.shapeColor = color(255, 0, 0);
     }
 
     if (deformation < 100) {
       car.shapeColor = color(0, 255, 0);
+      wall.shapeColor = color(0, 255, 0);
     }
 
     if (deformation < 180 && deformation > 100) {
       car.shapeColor = color(230, 230, 0);
+      wall.shapeColor = color(230, 230, 0);
     }
   }
 
